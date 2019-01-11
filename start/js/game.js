94ff0865cd2ea14a1f4a338548e514a4fe16d5d6
Adi Kantawala <adikan@ucla.edu>
2019-01-10 19:11:52 -0800

    Create Player and implement moving player left and right

---
+++ start/js/game.js
@@ -0,0 +1,48 @@
+const KEY_CODE_LEFT= 37;
+const KEY_CODE_RIGHT= 39;
+const KEY_CODE_SPACE= 32;
+
+
+const GAME_WIDTH = 800;
+const GAME_HEIGHT = 600;
+
+const GAME_STATE = {
+  playerX: 0,
+  playerY: 0
+};
+
+function setPosition($el, x, y) {
+  $el.style.transform = `translate(${x}px, ${y}px)`;
+}
+
+function createPlayer($container) {
+  GAME_STATE.playerX = GAME_WIDTH / 2;
+  GAME_STATE.playerY = GAME_HEIGHT - 50;
+  const $player = document.createElement("img");
+  $player.src = "img/player-blue-1.png";
+  $player.className = "player";
+  $container.appendChild($player);
+  setPosition($player, GAME_STATE.playerX, GAME_STATE.playerY);
+}
+
+function init() {
+  const $container = document.querySelector(".game");
+  createPlayer($container);
+
+}
+function onKeyDown(e){
+  if (e.keyCode === KEY_CODE_LEFT){
+    GAME_STATE.playerX -=5;
+    const $player = document.querySelector(".player");
+    setPosition($player, GAME_STATE.playerX, GAME_STATE.playerY);
+  }
+  else if (e.keyCode === KEY_CODE_RIGHT){
+    GAME_STATE.playerX +=5;
+    const $player = document.querySelector(".player");
+    setPosition($player, GAME_STATE.playerX, GAME_STATE.playerY);
+  }
+}
+
+init();
+
+window.addEventListener("keydown", onKeyDown)
