b840572afd2233b52e3ff40e04e51df4c0e02529
Adi Kantawala <adikan@ucla.edu>
2019-01-13 14:02:11 -0800

    Fix laser issue to correctly map space bar to firing laser

--- start/js/game.js
+++ start/js/game.js
@@ -6,10 +6,29 @@ const KEY_CODE_SPACE= 32;
 const GAME_WIDTH = 800;
 const GAME_HEIGHT = 600;
 
+const PLAYER_WIDTH = 20;
+const PLAYER_MAX_SPEED = 600;
+const LASER_MAX_SPEED = 300;
+
 const GAME_STATE = {
+  lastTime: Date.now(),
+  leftPressed: false,
+  rightPressed: false,
+  spacePressed: false,
   playerX: 0,
-  playerY: 0
+  playerY: 0,
+  lasers: [],
 };
+
+function clamp(v, min, max) {
+  if (v < min) {
+    return min;
+  } else if (v > max) {
+    return max;
+  } else {
+    return v;
+  }
+}
 
 function setPosition($el, x, y) {
   $el.style.transform = `translate(${x}px, ${y}px)`;
@@ -28,21 +47,82 @@ function createPlayer($container) {
 function init() {
   const $container = document.querySelector(".game");
   createPlayer($container);
-
 }
+
+function updatePlayer(dt, $container) {
+  if (GAME_STATE.leftPressed) {
+    GAME_STATE.playerX -= dt * PLAYER_MAX_SPEED;
+  }
+  if (GAME_STATE.rightPressed) {
+    GAME_STATE.playerX += dt * PLAYER_MAX_SPEED;
+  }
+  GAME_STATE.playerX = clamp(GAME_STATE.playerX, PLAYER_WIDTH, GAME_WIDTH - PLAYER_WIDTH);
+
+  if (GAME_STATE.spacePressed ) {
+     createLaser($container, GAME_STATE.playerX, GAME_STATE.playerY);
+   }
+
+  const $player = document.querySelector(".player");
+  setPosition($player, GAME_STATE.playerX, GAME_STATE.playerY);
+}
+
+function createLaser($container, x, y) {
+  const $element = document.createElement("img");
+  $element.src = "img/laser-blue-1.png";
+  $element.className = "laser";
+  $container.appendChild($element);
+  const laser = { x, y, $element };
+  GAME_STATE.lasers.push(laser);
+  const audio = new Audio("sound/sfx-laser1.ogg");
+  audio.play();
+  setPosition($element, x, y);
+}
+
+function updateLasers(dt, $container) {
+  const lasers = GAME_STATE.lasers;
+  for (let i = 0; i < lasers.length; i++) {
+    const laser = lasers[i]
+    laser.y -= dt * LASER_MAX_SPEED
+    setPosition(laser.$element, laser.x, laser.y);
+  }
+}
+
+function update(){
+  const currentTime = Date.now();
+  const dt = (currentTime - GAME_STATE.lastTime) / 1000
+
+  const $container = document.querySelector(".game");
+  updatePlayer(dt, $container);
+  updateLasers(dt, $container);
+
+  GAME_STATE.lastTime = currentTime;
+  window.requestAnimationFrame(update)
+}
+
 function onKeyDown(e){
   if (e.keyCode === KEY_CODE_LEFT){
-    GAME_STATE.playerX -=5;
-    const $player = document.querySelector(".player");
-    setPosition($player, GAME_STATE.playerX, GAME_STATE.playerY);
+    GAME_STATE.leftPressed = true;
   }
   else if (e.keyCode === KEY_CODE_RIGHT){
-    GAME_STATE.playerX +=5;
-    const $player = document.querySelector(".player");
-    setPosition($player, GAME_STATE.playerX, GAME_STATE.playerY);
+    GAME_STATE.rightPressed = true;
+  } else if (e.keyCode === KEY_CODE_SPACE) {
+    GAME_STATE.spacePressed = true;
+  }
+}
+function onKeyUp(e){
+  if (e.keyCode === KEY_CODE_LEFT){
+    console.log("left")
+    GAME_STATE.leftPressed = false;
+  }else if (e.keyCode === KEY_CODE_RIGHT){
+    GAME_STATE.rightPressed = false;
+  } else if (e.keyCode === KEY_CODE_SPACE) {
+    console.log("space")
+    GAME_STATE.spacePressed = false;
   }
 }
 
 init();
 
 window.addEventListener("keydown", onKeyDown)
+window.addEventListener("keyup", onKeyUp)
+window.requestAnimationFrame(update);
